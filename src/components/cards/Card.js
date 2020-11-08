a2e063fc13d9af71d329c1a7a786e14fef56f26b
Patrick Kirby <pkirbyinatx@gmail.com>
2020-11-08 15:47:58 -0600

    re-formatted Card.js

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,48 +1,37 @@
-import Axios from "axios";
 import React, { Component } from "react";
 
 export default class Card extends Component {
   state = {
     name: "",
-    image: "",
-    url: "",
-    pokemonData: "",
+    imageUrl: "",
     pokemonIndex: "",
   };
 
-  async componentDidMount() {
+  componentDidMount() {
     const { name, pokemonURL } = this.props;
-    this.setState({ name: name, url: pokemonURL });
-    const res = await Axios.get(this.state.url);
-    this.setState({ pokemonURL: res });
     const pokemonIndex = pokemonURL.split("/")[
       pokemonURL.split("/").length - 2
     ];
-    console.log(pokemonIndex);
-    this.setState({ pokemonIndex: pokemonIndex });
+    const imageUrl = `https://github.com/pokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+
+    console.log("index:", pokemonURL);
+
+    this.setState({
+      name,
+      pokemonIndex,
+      imageUrl,
+    });
   }
 
   render() {
-    //props
-    //set pokemon image size
-    const imageSize = {
-      height: "250px",
-      width: "250px",
-    };
     return (
-      <div className="col-xl-2 col-lg-4 mx-4 mt-4">
-        <img
-          className="card-img-top img-thumbnail rounded mx-auto d-block"
-          src="https://via.placeholder.com/150x150"
-          alt="pokemon"
-          style={imageSize}
-        />
-        <div className="card-body">
-          <h4 className="card-title text-center text-capitalize">
-            {this.state.name}
-          </h4>
-          <p className="card-text text-center">{this.state.url}</p>
-          <p>{this.state.pokemonIndex}</p>
+      <div className="col-md-3 col-sm-6 mb-5">
+        <div className="card">
+          <div className="card-header">{this.state.pokemonIndex}</div>
+          <img src={this.state.imageUrl} alt="" className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">{this.props.name}</p>
+          </div>
         </div>
       </div>
     );
