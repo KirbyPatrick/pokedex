7fc0e1318433586733105eb9438fff64dcf8110c
Patrick Kirby <pkirbyinatx@gmail.com>
2020-11-29 18:30:51 -0600

    pulled in and rendered height and weight, rounded

--- src/components/cards/Pokemon.js
+++ src/components/cards/Pokemon.js
@@ -14,6 +14,7 @@ export default class Pokemon extends Component {
     const { pokemonIndex } = this.props.match.params;
     //image URL
     const imageURL = `https://github.com/pokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+
     //Passing the api call the pokemonIndex value to state.
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`)
@@ -22,15 +23,29 @@ export default class Pokemon extends Component {
         const name = pokeData.forms[0].name;
         const id = pokeData.id;
         const types = pokeData.types[0].type.name;
-        this.setState({ name, id, types, imageURL });
+        const height = pokeData.height / 3;
+        const weight = pokeData.weight / 4.536;
+        const moves = pokeData.moves;
+        const stats = pokeData.stats;
+        this.setState({
+          name,
+          id,
+          types,
+          imageURL,
+          height,
+          weight,
+          moves,
+          stats,
+        });
       });
   }
-
   render() {
     return (
       <div className="container">
         <div className="row">
-          <h1 className="text-center boxy col-sm-12">{this.state.name}</h1>
+          <h1 className="text-center boxy col-sm-12">
+            {this.state.name.charAt(0).toUpperCase() + this.state.name.slice(1)}
+          </h1>
         </div>
         <div className="row d-flex boxy justify-content-center center-text">
           <img
@@ -39,10 +54,20 @@ export default class Pokemon extends Component {
             alt=""
           />
         </div>
-        <div className="row boxy">
-          <h2 className="col-sm-2 text-center">#{this.state.id}</h2>
-          <h2 className="col-sm-2 text-center">Types: {this.state.types}</h2>
+        <div className="row  d-flex justify-content-around">
+          <h2 className="col-sm-3  text-center boxy">ID# {this.state.id}</h2>
+          <h2 className="col-sm-3 text-center boxy">
+            Types: {this.state.types}
+          </h2>
+          <h2 className="col-sm-3 text-center boxy">
+            Height: {Math.round(this.state.height * 100) / 100} feet
+          </h2>
+          <h2 className="col-sm-3 text-center boxy">
+            Weight: {Math.round(this.state.weight)} lbs
+          </h2>
         </div>
+        <h1>Stats</h1>
+        <h1> </h1>
       </div>
     );
   }
